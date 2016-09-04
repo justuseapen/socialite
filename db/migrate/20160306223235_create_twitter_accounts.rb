7b540dbd958988038bef25336bc5f9e654b65274
Justus <justuseapen@gmail.com>
2016-09-04 11:47:11 -0400

    Updated list show view with avatars and real names. Made cards and edited migrations to accommodate avatar urls and names

--- db/migrate/20160306223235_create_twitter_accounts.rb
+++ db/migrate/20160306223235_create_twitter_accounts.rb
@@ -3,6 +3,8 @@ class CreateTwitterAccounts < ActiveRecord::Migration
     create_table :twitter_accounts do |t|
       t.integer :list_id
       t.string :handle, null: false
+      t.string :name
+      t.string :avatar_url
 
       t.timestamps null: false
     end
