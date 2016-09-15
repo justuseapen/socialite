a2661f57038e6dd972b5141111cb894cef11051a
Justus <justuseapen@gmail.com>
2016-09-15 10:24:45 +0200

    require lists to belong to a buffer profile using presence validation in list model. Updated params in list controller to allow buffer profile reference. Finally, added buffer_profile selection to new list view.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,10 @@
 class List < ActiveRecord::Base
 	has_many :twitter_accounts
 	belongs_to :buffer_profile
+	validates :buffer_profile, presence: true
 
 	def top_tweets(percentage_integer)
-		puts "Pulling top tweets"
+		puts "Pulling top #{percentage_integer}% tweets"
 		tweets = []
 		twitter_accounts.each do |ta|
 			tweets = tweets + ta.top_tweets(percentage_integer)
