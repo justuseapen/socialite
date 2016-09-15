6395ff0885401d046557428d98138d08adddd6bb
Justus <justuseapen@gmail.com>
2016-09-15 10:22:01 +0200

    corrected flash messages on buffer profile controller

--- app/controllers/buffer_profiles_controller.rb
+++ app/controllers/buffer_profiles_controller.rb
@@ -5,14 +5,14 @@ class BufferProfilesController < ApplicationController
 	def create
 		p = BufferProfile.create(profile_params)
 		if p.save
-			flash[:success] = "You've added #{p.formatted_username} to Zetool!"
+			flash[:success] = "You've added #{p.formatted_username}!"
 			redirect_to new_buffer_profiles_path
 		end
 	end
 	def destroy
 		@buffer_profile = BufferProfile.find(params[:id])
 		@buffer_profile.destroy
-		flash[:success] = "You've removed #{@buffer_profile.formatted_username} from Zetool!"
+		flash[:success] = "You've removed #{@buffer_profile.formatted_username}!"
 		redirect_to action: :new
 	end
 	private
