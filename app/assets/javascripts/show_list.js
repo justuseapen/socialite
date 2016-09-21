dd1e751ba0997524c0ff8d7019c7696e98acf956
Justus <justuseapen@gmail.com>
2016-09-21 10:23:21 +0200

    added js function to display success and failure messages after adding tweets to buffer

--- app/assets/javascripts/show_list.js
+++ app/assets/javascripts/show_list.js
@@ -1,5 +1,46 @@
 var listId = parseInt($(".list").attr('id'))
 
+function flashUpdatesStatus(response){
+	if($('.alerts')){ $('.alerts').remove() }
+
+	$('nav').after("\
+			<div class=\"alerts\"></div>\
+		")
+
+	var numberOfSuccessfulUpdates = $(response.successful_updates).length
+	var numberOfFailedUpdates = $(response.failed_updates).length
+	
+	// set the success message
+	if($(response.successful_updates).length > 1){
+		var successMessage = numberOfSuccessfulUpdates + " tweets were successfully buffered."
+	} else {
+		var successMessage = numberOfSuccessfulUpdates + " tweet was successfully buffered."
+	}
+
+	// set the failure message
+	if($(response.failed_updates).length > 1){
+		var failMessage = numberOfFailedUpdates + " tweets failed to buffer. See red tweets below for more information."
+	} else {
+		var failMessage = numberOfFailedUpdates + " tweet failed to buffer. See tweet in red below for more information."
+	}
+
+	// display flash messages
+	if(numberOfSuccessfulUpdates > 0){
+		$('.alerts').append(
+			"<div class=\"alert alert-success\">" + successMessage + "</div>"
+		);
+	}
+
+	if(numberOfFailedUpdates > 0){
+		$('.alerts').append(
+			"<div class=\"alert alert-danger\">" + failMessage + "</div>"
+		);
+	}
+
+	// scroll to top so the user sees the messages
+	window.scrollTo(0,0);
+}
+
 function addTweetsToBufferQueue(tweets) {
 	$.ajax({
 	    url: '/updates',
@@ -10,7 +51,7 @@ function addTweetsToBufferQueue(tweets) {
 	    },
 	    dataType: 'json',
 	    success: function(response){
-	    	console.log("POSTed updates");
+	    	flashUpdatesStatus(response);
 	    }
 		})
 }
