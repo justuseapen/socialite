3d7fe1ac947607cfc92ece2b086efab515bf7eb8
Justus <justuseapen@gmail.com>
2016-09-27 14:20:55 +0200

    added validations, associations, and commentary to buffer_profile model

--- app/models/buffer_profile.rb
+++ app/models/buffer_profile.rb
@@ -1,19 +1,35 @@
 class BufferProfile < ActiveRecord::Base
+	
+
+	# Buffer Profile
+	# ==============
+	# buffer_account_id : integer
+	# formatted_username : string
+	# buffer_id : string
+	# avatar_url : string
+
+
 	has_many :lists
 	belongs_to :buffer_account
+	has_one :user, through: :buffer_account
 	validates :buffer_account_id, presence: true
-	attr_accessor :active
+	validates :formatted_username, presence: true
+	validates :buffer_id, presence: true
+	validates :avatar_url, presence: true
 
 	def client
 		buffer_account.client
 	end
 
+	# Change to "updates"
 	def update_queue_with(tweetIds)
 		puts "updating buffer queue"
 		updates = []
 		successful_updates = []
 		failed_updates = []
+		# pulls tweets with given ids
 		tweets = $twitter.statuses(tweetIds)
+		# buffers each tweet
 		tweets.each do |tweet|
 			puts "buffering #{ tweet.text }"
 			if tweet.media[0].blank?
